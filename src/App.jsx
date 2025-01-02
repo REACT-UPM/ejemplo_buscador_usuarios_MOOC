279b70875d72f6a45defbca0f1c48caf14bff3f7
Enrique <enrique.barra@upm.es>
2025-01-02 16:20:45 +0100

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Resultados from "./Resultados";
+import Header from "./Header";
+import './App.css';
+import {mock1} from "./constants/users.js";
+import CONFIG from "./config/config";
 
+const USE_SERVER = CONFIG.use_server;
+
+//ideas:
+//probar response.status y si no es 200 poner un error en pantalla
+//
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [resultado, setResultado] = useState(null);
+
+  const callServer = async (param) => {    
+      if(USE_SERVER) {
+        try {
+          let queryparams = "";
+          if(param==="all"){
+            queryparams = "?limit=" + CONFIG.num_items;
+          } else {
+            queryparams = "/search?q=" + query;
+          }
+          const response = await fetch(`${CONFIG.server_url}${queryparams}`);
+          const data = await response.json();         
+          //console.log(data);
+          setResultado(data.users);
+        } catch (error) {
+          console.log(error);
+          setResultado({ error: {description: error.message} });
+        }
+      } else {
+        //console.log(mock1.users)
+        setResultado(mock1.users);
+      }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="main">
+        <Header />
+ 				<h2 id="buscador">Buscador de usuarios</h2>
+				<div><input type="text" id="query" placeholder="Texto a buscar" value={query} onChange={e=>setQuery(e.target.value)}></input></div>
+				<br/>
+        <button id="botonsearch" className="new" onClick={()=>callServer()}>
+				  Buscar
+				</button> 
+        <button id="botonall" className="new" onClick={()=>callServer("all")}>
+				  Ver Todos
+				</button>        		
+        {resultado && <Resultados numitems={CONFIG.num_items} resultado={resultado} />}	
+			</div>
+  );
 }
 
-export default App
+export default App;
